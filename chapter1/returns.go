6152cfaa890390c25818143650fd38baf1a85ad1
alexander <mutabored@ya.ru>
2017-08-01 23:28:42 +0700

    1. Присваивание значений нскольким переменным 2. Просто обмен значениям переменных 3. Использование временной переменной

--- chapter1/returns.go
+++ chapter1/returns.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 )
 
-func Names() (string, string) {
-	return "Foo", "Bar"
+func Names() (string, string) { // простая функция,
+	return "Foo", "Bar" // возвращающая два строковых значения
 }
 
 func main() {
-	n1, n2 := Names()
+	n1, n2 := Names() // приваивание двум переменным по порядку двух значчениий
+	fmt.Println(n1, n2)
+	n1, n2 = n2, n1
 	fmt.Println(n1, n2)
 
-	n3, _ := Names()
+	n3, _ := Names() // использование _ в качестве никогда не используемой переменной
 	fmt.Println(n3)
+	// n4  := Names() а такая запись будет ошибкой. Names возвращает два значения,
+	// ни как не утискивающиеся в одну переменную
 }
